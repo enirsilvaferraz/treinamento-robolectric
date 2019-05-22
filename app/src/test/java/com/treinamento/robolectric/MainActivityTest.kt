8fed7dfbc448cba40076c2dd4f5ae24397ee5b75
Guilherme Rogério Toquete <gtoquete@ciandt.com>
2019-05-22 15:08:25 -0300

    Testes robolectric

--- app/src/test/java/com/treinamento/robolectric/MainActivityTest.kt
+++ app/src/test/java/com/treinamento/robolectric/MainActivityTest.kt
@@ -10,6 +10,7 @@ import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows
 import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowToast
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
@@ -43,36 +44,49 @@ class MainActivityTest {
 
     @Test
     fun `Deve exibir o texto informe o nome do usuario`() {
-
+        assertEquals("Informe o nome do usuário", activity.login_label_username.text)
     }
 
     @Test
     fun `Deve exibir o texto informe a senha`() {
-
+        assertEquals("Informe a senha", activity.login_label_password.text)
     }
 
     @Test
     fun `Deve exibir o botao com o texto login`() {
-
+        assertEquals("Login", activity.login_buttom.text)
     }
 
     @Test
     fun `Deve exibir o botao de login desabilitado quando os campos nao tiverem preenchidos`() {
-
+        assertEquals(false, activity.login_buttom.isEnabled)
     }
 
     @Test
     fun `Deve exibir o botao de login habilitado quando os campos tiverem preenchidos`() {
+        activity.login_field_username.setText("bla")
+        activity.login_field_password.setText("bla")
 
+        assertEquals(true, activity.login_buttom.isEnabled)
     }
 
     @Test
     fun `Deve exibir mensagem de usuario invalido`() {
+        activity.login_field_username.setText("blabla")
+        activity.login_field_password.setText("wrfwrfe")
 
+        activity.login_buttom.performClick()
+
+        assertEquals("Usuario invalido", ShadowToast.getTextOfLatestToast())
     }
 
     @Test
     fun `Deve exibir mensagem de senha errada`() {
+        activity.login_field_username.setText("username")
+        activity.login_field_password.setText("wrfwrfe")
 
+        activity.login_buttom.performClick()
+
+        assertEquals("A senha está errada", ShadowToast.getTextOfLatestToast())
     }
 }
