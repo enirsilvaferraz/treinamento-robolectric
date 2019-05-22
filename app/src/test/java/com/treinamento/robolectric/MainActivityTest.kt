02d7641d25b587bbbbd25cae23985e433b1afcde
Gabriel Bombicino Kimura <gkimura@MACCIT016841.local>
2019-05-22 15:06:03 -0300

    Testes da view

--- app/src/test/java/com/treinamento/robolectric/MainActivityTest.kt
+++ app/src/test/java/com/treinamento/robolectric/MainActivityTest.kt
@@ -1,6 +1,5 @@
 package com.treinamento.robolectric
 
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import org.junit.Assert.*
 import org.junit.Before
@@ -9,12 +8,12 @@ import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows
-import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowToast
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
 
-    lateinit var activity:MainActivity
+    lateinit var activity: MainActivity
 
     @Before
     fun setUp() {
@@ -22,13 +21,13 @@ class MainActivityTest {
     }
 
     @Test
-    fun `Deve exibir o texto Tela de Login`(){
+    fun `Deve exibir o texto Tela de Login`() {
 
-        assertEquals("Tela de Login", activity.login_title.text.toString())
+        assertEquals("Tela de Login", activity.login_title.text)
     }
 
     @Test
-    fun `Deve abrir a tela de lista em caso de login com sucesso`(){
+    fun `Deve abrir a tela de lista em caso de login com sucesso`() {
 
         activity.login_field_username.setText("username")
         activity.login_field_password.setText("12345")
@@ -44,35 +43,58 @@ class MainActivityTest {
     @Test
     fun `Deve exibir o texto informe o nome do usuario`() {
 
+        assertEquals("Informe o nome do usuário", activity.login_label_username.text)
     }
 
     @Test
     fun `Deve exibir o texto informe a senha`() {
 
+        assertEquals("Informe a senha", activity.login_label_password.text)
     }
 
     @Test
     fun `Deve exibir o botao com o texto login`() {
 
+        assertEquals("Login", activity.login_buttom.text)
     }
 
     @Test
     fun `Deve exibir o botao de login desabilitado quando os campos nao tiverem preenchidos`() {
 
+        activity.login_field_username.setText("")
+        activity.login_field_password.setText("")
+
+        assertFalse(activity.login_buttom.isEnabled)
     }
 
     @Test
     fun `Deve exibir o botao de login habilitado quando os campos tiverem preenchidos`() {
 
+        activity.login_field_username.setText("username")
+        activity.login_field_password.setText("12345")
+
+        assertTrue(activity.login_buttom.isEnabled)
     }
 
     @Test
     fun `Deve exibir mensagem de usuario invalido`() {
 
+        activity.login_field_username.setText("emanresu")
+        activity.login_field_password.setText("54321")
+
+        activity.login_buttom.performClick()
+
+        assertEquals("Usuario invalido", ShadowToast.getTextOfLatestToast())
     }
 
     @Test
     fun `Deve exibir mensagem de senha errada`() {
 
+        activity.login_field_username.setText("username")
+        activity.login_field_password.setText("54321")
+
+        activity.login_buttom.performClick()
+
+        assertEquals("A senha está errada", ShadowToast.getTextOfLatestToast())
     }
 }
